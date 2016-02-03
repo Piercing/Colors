07cc4acbec32caa8a395e89caac19f8b5369d37f
Piercing <merlosalbarracin@gmail.com>
2016-02-04 00:25:18 +0100

    start

---
+++ CollectionSample/MGCColors.h
@@ -0,0 +1,13 @@
+//
+//  MGCColors.h
+//  CollectionSample
+//
+//  Created by MacBook Pro on 4/2/16.
+//  Copyright © 2016 weblogmerlos.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGCColors : NSObject
+
+@end
---
+++ CollectionSample/MGCColors.m
@@ -0,0 +1,13 @@
+//
+//  MGCColors.m
+//  CollectionSample
+//
+//  Created by MacBook Pro on 4/2/16.
+//  Copyright © 2016 weblogmerlos.com. All rights reserved.
+//
+
+#import "MGCColors.h"
+
+@implementation MGCColors
+
+@end
